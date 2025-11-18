e98daf260ddeb023512bf543f9a93b0b748953d1
Dhanush-uno <dhanushmarupaka075@gmail.com>
2025-11-18 08:51:32 +0530

    rh

--- routes/snippet.js
+++ routes/snippet.js
@@ -34,14 +34,20 @@ router.get("/", protect, async (req, res) => {
 // DELETE snippet
 router.delete("/:id", protect, async (req, res) => {
   try {
-    const snippet = await Snippet.findOneAndDelete({
-      _id: req.params.id,
-      userId: req.user._id
-    });
+    const snippet = await Snippet.findById(req.params.id);
 
-    if (!snippet) return res.status(404).json({ msg: "Snippet not found" });
+    if (!snippet) {
+      return res.status(404).json({ msg: "Snippet not found" });
+    }
 
+    // only owner can delete
+    if (snippet.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ msg: "Not allowed" });
+    }
+
+    await snippet.deleteOne();
     res.json({ msg: "Snippet deleted successfully" });
+
   } catch (err) {
     res.status(500).json({ msg: "Server error" });
   }
